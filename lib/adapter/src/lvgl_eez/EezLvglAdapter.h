97923f3f0982a43b2c1f8ca2e57df9dfbcf21972
i12309 <i12309@bk.ru>
2026-04-19 17:55:49 +0300

    Add enableLvglObjectHelpers to EezLvglHooks

--- lib/adapter/src/lvgl_eez/EezLvglAdapter.h
+++ lib/adapter/src/lvgl_eez/EezLvglAdapter.h
@@ -15,6 +15,8 @@ struct EezLvglHooks {
     bool (*setValue)(void* userData, void* uiObject, int32_t value) = nullptr;
     bool (*setVisible)(void* userData, void* uiObject, bool visible) = nullptr;
     bool (*setColor)(void* userData, void* uiObject, uint32_t bgColor, uint32_t fgColor) = nullptr;
+    // Включает fallback-хелперы для стандартных LVGL объектов, если hook отсутствует или вернул false.
+    bool enableLvglObjectHelpers = true;
 
     // Опциональный poll/flush входных событий UI.
     // Может быть no-op, если UI callbacks публикуют события сразу.
